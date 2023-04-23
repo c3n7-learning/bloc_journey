45e666ea84762ed2ba718b394d8afbdcbafcf459
Timothy Karani <kartimothy@gmail.com>
2023-04-23 12:00:10 +0300

    Done with flutter infinite list

---
+++ flutter_infinite_list/lib/posts/posts.dart
@@ -0,0 +1,3 @@
+export 'models/models.dart';
+export 'bloc/post_bloc.dart';
+export 'view/view.dart';
---
+++ flutter_infinite_list/lib/posts/view/posts_list.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_infinite_list/posts/posts.dart';
+
+class PostsList extends StatefulWidget {
+  const PostsList({super.key});
+
+  @override
+  State<PostsList> createState() => _PostsListState();
+}
+
+class _PostsListState extends State<PostsList> {
+  final _scrollController = ScrollController();
+
+  bool get _isBottom {
+    if (!_scrollController.hasClients) return false;
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.offset;
+    return currentScroll >= (maxScroll * 0.9);
+  }
+
+  void _onScroll() {
+    if (_isBottom) context.read<PostBloc>().add(PostFetched());
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController
+      ..removeListener(_onScroll)
+      ..dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<PostBloc, PostState>(builder: (context, state) {
+      switch (state.status) {
+        case PostStatus.failure:
+          return const Center(child: Text('failed to fetch posts'));
+        case PostStatus.success:
+          if (state.posts.isEmpty) {
+            return const Center(child: Text('no posts'));
+          }
+
+          return ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return index >= state.posts.length
+                  ? const BottomLoader()
+                  : PostListItem(post: state.posts[index]);
+            },
+            itemCount: state.hasReachedMax
+                ? state.posts.length
+                : state.posts.length + 1,
+            controller: _scrollController,
+          );
+        case PostStatus.initial:
+          return const Center(child: CircularProgressIndicator());
+      }
+    });
+  }
+}
+
+class BottomLoader extends StatelessWidget {
+  const BottomLoader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: SizedBox(
+        height: 24,
+        width: 24,
+        child: CircularProgressIndicator(
+          strokeWidth: 1.5,
+        ),
+      ),
+    );
+  }
+}
+
+class PostListItem extends StatelessWidget {
+  final Post post;
+
+  const PostListItem({super.key, required this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Material(
+      child: ListTile(
+        leading: Text('${post.id}', style: textTheme.bodySmall),
+        title: Text(post.title),
+        isThreeLine: true,
+        subtitle: Text(post.body),
+        dense: true,
+      ),
+    );
+  }
+}
